7905415a532f49a4f7a02b4924566bdb45541152
Nathan McCartney <nmcCartney@winsupers-MBP.fios-router.home>
2016-09-10 21:41:47 -0400

    animation test

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 (function () {
   'use strict';
 
-  console.log('app..')
-
   /* @ngdoc object
    * @name polyTest
    * @requires $urlRouterProvider
@@ -22,8 +20,25 @@
     .config(config);
 
   function config($urlRouterProvider) {
-    console.log('configging..')
     $urlRouterProvider.otherwise('/');
   }
 
 })();
+
+$ = require('jquery')
+
+$(document).ready(function(){
+  console.log('add btns');
+  $('#btn-panel-1').on('click',function(){
+    $('#panel-3').addClass('left')
+    $('#panel-3').removeClass('middle')
+  })
+  $('#btn-panel-2').on('click',function(){
+    $('#panel-3').removeClass('left')
+    $('#panel-3').addClass('middle')
+  })
+  $('#btn-panel-3').on('click',function(){
+    $('#panel-3').removeClass('left')
+    $('#panel-3').removeClass('middle')
+  })
+})
